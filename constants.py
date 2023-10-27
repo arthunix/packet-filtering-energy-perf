4b01535f506387fe62073983a4c815198f2e52f8
Arthur - DC <thursilverio@gmail.com>
2023-10-27 12:34:17 -0300

    python constants

---
+++ constants.py
@@ -0,0 +1,9 @@
+EXECUTE_FOR_TIME = '5'     # in seconds
+EXECUTE_PERF_FOR_TIME = '5' # in seconds
+
+DNETIF = 'enp5s0f0'
+SNETIF = 'enp5s0f1'
+DADDR = '127.0.0.1'
+SADDR = '127.0.0.1'
+DPORT = '12345'
+SPORT = '12345'
